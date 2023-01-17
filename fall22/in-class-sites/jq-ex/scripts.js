468c0a66c43ae85f6bd56d37c28ee6b2f7c17adc
Aidan Quinlan <aidankquinlan@gmail.com>
2023-01-17 13:51:39 -0800

    -

---
+++ fall22/in-class-sites/jq-ex/scripts.js
@@ -0,0 +1,21 @@
+$(".dark-btn").click(function(){
+  $("body").toggleClass('dark');
+  $(".box").toggleClass('dark');
+});
+
+$(".glow-btn").click(function(){
+  $(".box").toggleClass('glow');
+});
+
+$(".spin-btn").click(function(){
+  $(".box").toggleClass('spin');
+});
+
+$(".reveal-btn").click(function(){
+  $(".chair").css('opacity', '1');
+  $(".reveal-btn").hide();
+});
+
+$( function() {
+  $( ".draggable" ).draggable();
+} );
