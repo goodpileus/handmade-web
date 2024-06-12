9fb2352e881ed9f496cb24c4caf823bb8d432dd1
Aidan Quinlan <aidankquinlan@gmail.com>
2024-06-11 18:28:24 -0700

    -

---
+++ spring24/in-class-sites/jquery/scripts.js
@@ -0,0 +1,27 @@
+$(".dark-btn").click(
+  function(){
+    $(".box").toggleClass("dark");
+    $("body").toggleClass("dark");
+  }
+);
+
+$(".spin-btn").click(
+  function(){
+    $(".box").toggleClass("spin");
+  }
+);
+
+$(".reveal-btn").click(
+  function(){
+    // $(".chair").addClass("reveal");
+    $(".chair").css("display", "block");
+    // $(".reveal-btn").css("display", "none");
+    $(".reveal-btn").hide();
+  }
+);
+
+$( function() {
+  $( ".draggable" ).draggable({ 
+    containment: ".container"
+  });
+} );
