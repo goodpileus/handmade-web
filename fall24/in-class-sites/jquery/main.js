8d61752396f84ca9b3a4b65f0601db099cc3f9ff
Aidan Quinlan <aidankquinlan@gmail.com>
2025-01-23 14:18:13 -0800

    homepage text update

---
+++ fall24/in-class-sites/jquery/main.js
@@ -0,0 +1,24 @@
+$(".dark-btn").click(
+  function(){
+    $("body").toggleClass("dark");
+    $(".box").toggleClass("dark");
+  }
+);
+
+$(".spin-btn").click(
+  function(){
+    $(".box").toggleClass("spin");
+  }
+);
+
+$(".reveal-btn").click(
+  function(){
+    $(".chair").addClass("reveal");
+    // $(".reveal-btn").css("display", "none");
+    $(".reveal-btn").hide();
+  }
+);
+
+$(".draggable").draggable({ 
+  snap: true
+});
