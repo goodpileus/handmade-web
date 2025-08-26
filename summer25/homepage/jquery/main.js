71f48e2e93e2b297e9e99cae6143cbf701dd4ea9
Aidan Quinlan <aidankquinlan@gmail.com>
2025-08-25 22:14:17 -0700

    week 2

---
+++ summer25/homepage/jquery/main.js
@@ -0,0 +1,31 @@
+
+$(".dark-btn").click(
+    function(){
+        $(".box").toggleClass("dark");
+        $("body").toggleClass("dark");
+    }
+);
+
+$(".spin-btn").click(
+    function(){
+        $(".box").toggleClass("spin");
+    }
+);
+
+$(".reveal-btn").click(
+    function(){
+        // $(".chair").addClass("reveal");
+        // $(".chair").css("display", "block");
+        $(".chair").show();
+
+        // $(".reveal-btn").css("display", "none");
+        $(".reveal-btn").hide();
+    }
+);
+
+$( ".draggable" ).draggable({
+    snap:true,
+    containment:".container",
+    scroll:false,
+    stack:".draggable"
+});
